9da5c54d6b0cb2ee7dc26e681e5602fe9103c8a8
napoorna <napoorna@gmail.com>
2020-02-27 10:19:27 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
+//include the required modules
 const path = require('path');
 const express = require('express')
 const layout = require('express-layout')
 const hbs = require('hbs')
 const routes = require('./routes')
 
+//Setting the port number to run the application
 const port = process.env.PORT || 3535;
 
 const app = express()
 
+//Perses the http request body
 const bodyParser = require('body-parser')
 
+//Setting up the template path
 const viewPath= path.join(__dirname, 'views');
 const partialsPath= path.join(__dirname, 'views/partials');
 
@@ -23,6 +27,7 @@ const middlewares = [
     bodyParser.urlencoded({extended:false})
 ]
 
+//App use to add the middleware properties
 app.use(middlewares)
 
 app.use('/', routes)
